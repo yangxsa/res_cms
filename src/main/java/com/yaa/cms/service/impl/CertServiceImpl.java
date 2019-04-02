853e23aa875af19b270983416b42e12c1633a15e
yangxs <yanghbwork@163.com>
2019-04-02 16:27:59 +0800

    二维码

--- src/main/java/com/yaa/cms/service/impl/CertServiceImpl.java
+++ src/main/java/com/yaa/cms/service/impl/CertServiceImpl.java
@@ -1,7 +1,11 @@
 package com.yaa.cms.service.impl;
 
+import com.yaa.cms.util.ExcelUtil;
 import com.yaa.cms.util.Result;
 import org.apache.commons.lang3.StringUtils;
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -55,19 +59,38 @@ public class CertServiceImpl implements CertService {
 
     @Override
     public Result importExcel(MultipartFile file) {
-        Long size = file.getSize();
-        String fileType = file.getContentType();
-        if(file == null){
-            return Result.error("文件不能为空");
+        try {
+            Long size = file.getSize();
+            String fileType = file.getContentType();
+            if (file == null) {
+                return Result.error("文件不能为空");
+            }
+            // 文件大小超过2M
+            if (size > 2097152) {
+                return Result.error("资源解析失败,导入文件超过2M！");
+            }
+            Workbook workbook = null;
+            if (fileType.equals("application/vnd.openxmlformats-officedocument.spreadsheetml.sheet")) {
+                workbook = new XSSFWorkbook(file.getInputStream());
+            }else if(fileType.equals("application/vnd.ms-excel")){
+                workbook = new HSSFWorkbook(file.getInputStream());
+            }else{
+                return Result.error("仅支持xls,xlsx文件");
+            }
+            List<Cert> list = ExcelUtil.importXlsCert(workbook);
+            if(list == null){
+                return Result.error("资源文件加载失败!");
+            }
+            if(list != null && list.size()>0){
+                for(Cert cert : list){
+                    certDao.saveCert(cert);
+                }
+            }
+            return Result.ok();
+        } catch (Exception e) {
+            e.getStackTrace();
         }
-        // 文件大小超过2M
-        if (size > 2097152) {
-            return Result.error("资源解析失败,导入文件超过2M！");
-        }
-        if(!fileType.contains("xls")){
-            return Result.error("仅支持xls文件");
-        }
-        return Result.ok();
+        return Result.error();
     }
 
 }
---
+++ src/main/java/com/yaa/cms/util/ExcelUtil.java
@@ -0,0 +1,270 @@
+package com.yaa.cms.util;
+
+import com.yaa.cms.model.Cert;
+import org.apache.poi.hssf.usermodel.*;
+import org.apache.poi.hssf.util.HSSFColor;
+import org.apache.poi.poifs.filesystem.POIFSFileSystem;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ExcelUtil {
+
+    //导出表的标题
+    private String title = "records";
+    //导出表的列名
+    private String[] rowName;
+
+    private List<Object[]> dataList = new ArrayList<Object[]>();
+
+    private HttpServletResponse response;
+
+    //构造方法，传入要导出的数据
+    public ExcelUtil(String[] rowName, List<Object[]> dataList, HttpServletResponse response) {
+        this.dataList = dataList;
+        this.rowName = rowName;
+        this.response = response;
+    }
+
+    public static List<Cert> importXlsCert(Workbook workbook) {
+        try {
+            List<Cert> list = new ArrayList<>();
+            //1.获取表
+            Sheet sheet = workbook.getSheetAt(0);
+            //2.获取sheet中一共有多少行，遍历行（
+            int physicalNumberOfRows = sheet.getPhysicalNumberOfRows();
+            //第一行是标题(从1开始)
+            for (int i = 1; i < physicalNumberOfRows; i++) {
+                Row row = sheet.getRow(i);
+                int physicalNumberOfCells = row.getPhysicalNumberOfCells();
+                if(physicalNumberOfCells == 9){
+                    Cert cert = new Cert();
+                    cert.setNavicert(row.getCell(0).getStringCellValue());
+                    cert.setApprovalTime(row.getCell(1).getStringCellValue());
+                    cert.setNumber(row.getCell(2).getStringCellValue());
+                    cert.setOwned(row.getCell(3).getStringCellValue());
+                    cert.setProjectName(row.getCell(4).getStringCellValue());
+                    cert.setProjectAddress(row.getCell(5).getStringCellValue());
+                    cert.setAccommodation(row.getCell(6).getStringCellValue());
+                    cert.setAccommodationAddress(row.getCell(7).getStringCellValue());
+                    cert.setPermit(row.getCell(8).getStringCellValue());
+                    list.add(cert);
+                }else{
+                    return null;
+                }
+            }
+            return list;
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    /*
+     * 导出数据
+     * */
+    public void export() throws Exception {
+        try {
+            // 创建工作簿对象
+            HSSFWorkbook workbook = new HSSFWorkbook();
+            // 创建工作表
+            HSSFSheet sheet = workbook.createSheet(title);
+
+            // 产生表格标题行
+//            HSSFRow rowm = sheet.createRow(0);
+//            HSSFCell cellTiltle = rowm.createCell(0);
+
+            //sheet样式定义【getColumnTopStyle()/getStyle()均为自定义方法 - 在下面  - 可扩展】
+            //获取列头样式对象
+            HSSFCellStyle columnTopStyle = this.getColumnTopStyle(workbook);
+            //单元格样式对象
+            HSSFCellStyle style = this.getStyle(workbook);
+
+//            sheet.addMergedRegion(new CellRangeAddress(0, 1, 0, (rowName.length - 1)));
+//            cellTiltle.setCellStyle(columnTopStyle);
+//            cellTiltle.setCellValue(title);
+
+            // 定义所需列数
+            int columnNum = rowName.length;
+            // 在索引0的位置创建行(最顶端的行)
+            HSSFRow rowRowName = sheet.createRow(0);
+
+            // 将列头设置到sheet的单元格中
+            for (int n = 0; n < columnNum; n++) {
+                //创建列头对应个数的单元格
+                HSSFCell cellRowName = rowRowName.createCell(n);
+                //设置列头单元格的数据类型
+                cellRowName.setCellType(HSSFCell.CELL_TYPE_STRING);
+                HSSFRichTextString text = new HSSFRichTextString(rowName[n]);
+                //设置列头单元格的值
+                cellRowName.setCellValue(text);
+                //设置列头单元格样式
+                cellRowName.setCellStyle(columnTopStyle);
+            }
+
+            //将查询出的数据设置到sheet对应的单元格中
+            for (int i = 0; i < dataList.size(); i++) {
+                //遍历每个对象
+                Object[] obj = dataList.get(i);
+                //创建所需的行数(从1开始 第0行是表头)
+                HSSFRow row = sheet.createRow(i + 1);
+
+                for (int j = 0; j < obj.length; j++) {
+                    //设置单元格的数据类型
+                    HSSFCell cell = null;
+                    cell = row.createCell(j, HSSFCell.CELL_TYPE_STRING);
+                    if (!"".equals(obj[j]) && obj[j] != null) {
+                        //设置单元格的值
+                        cell.setCellValue(obj[j].toString());
+                    } else {
+                        //设置单元格的值(为空)
+                        cell.setCellValue("");
+                    }
+                    cell.setCellStyle(style);//设置单元格样式
+                }
+            }
+            //让列宽随着导出的列长自动适应
+            for (int colNum = 0; colNum < columnNum; colNum++) {
+                int columnWidth = sheet.getColumnWidth(colNum) / 256;
+                for (int rowNum = 0; rowNum < sheet.getLastRowNum(); rowNum++) {
+                    HSSFRow currentRow;
+                    //当前行未被使用过
+                    if (sheet.getRow(rowNum) == null) {
+                        currentRow = sheet.createRow(rowNum);
+                    } else {
+                        currentRow = sheet.getRow(rowNum);
+                    }
+                    if (currentRow.getCell(colNum) != null) {
+                        HSSFCell currentCell = currentRow.getCell(colNum);
+                        if (currentCell.getCellType() == HSSFCell.CELL_TYPE_STRING) {
+                            int length = currentCell.getStringCellValue().getBytes().length;
+                            if (columnWidth < length) {
+                                columnWidth = length;
+                            }
+                        }
+                    }
+                }
+                if (colNum == 0) {
+                    sheet.setColumnWidth(colNum, (columnWidth - 2) * 256);
+                } else {
+                    sheet.setColumnWidth(colNum, (columnWidth + 4) * 256);
+                }
+            }
+
+            if (workbook != null) {
+                try {
+                    String fileName = "Excel-" + String.valueOf(System.currentTimeMillis()).substring(4, 13) + ".xls";
+                    String headStr = "attachment; filename=\"" + fileName + "\"";
+                    response.setContentType("APPLICATION/OCTET-STREAM");
+                    response.setHeader("Content-Disposition", headStr);
+                    OutputStream out = response.getOutputStream();
+                    workbook.write(out);
+                    out.flush();
+                    out.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * 列头单元格样式
+     * @param workbook
+     * @return
+     */
+    public HSSFCellStyle getColumnTopStyle(HSSFWorkbook workbook) {
+        // 设置字体
+        HSSFFont font = workbook.createFont();
+        //设置字体大小
+        font.setFontHeightInPoints((short) 11);
+        //字体加粗
+        font.setBoldweight(HSSFFont.BOLDWEIGHT_BOLD);
+        //设置字体名字
+        font.setFontName("宋体");
+        //设置样式;
+        HSSFCellStyle style = workbook.createCellStyle();
+        //设置底边框;
+        style.setBorderBottom(HSSFCellStyle.BORDER_THIN);
+        //设置底边框颜色;
+        style.setBottomBorderColor(HSSFColor.BLACK.index);
+        //设置左边框;
+        style.setBorderLeft(HSSFCellStyle.BORDER_THIN);
+        //设置左边框颜色;
+        style.setLeftBorderColor(HSSFColor.BLACK.index);
+        //设置右边框;
+        style.setBorderRight(HSSFCellStyle.BORDER_THIN);
+        //设置右边框颜色;
+        style.setRightBorderColor(HSSFColor.BLACK.index);
+        //设置顶边框;
+        style.setBorderTop(HSSFCellStyle.BORDER_THIN);
+        //设置顶边框颜色;
+        style.setTopBorderColor(HSSFColor.BLACK.index);
+        //在样式用应用设置的字体;
+        style.setFont(font);
+        //设置自动换行;
+        style.setWrapText(false);
+        //设置水平对齐的样式为居中对齐;
+        style.setAlignment(HSSFCellStyle.ALIGN_CENTER);
+        //设置垂直对齐的样式为居中对齐;
+        style.setVerticalAlignment(HSSFCellStyle.VERTICAL_CENTER);
+
+        return style;
+
+    }
+
+    /**
+     * 列数据信息单元格样式
+     * @param workbook
+     * @return
+     */
+    public HSSFCellStyle getStyle(HSSFWorkbook workbook) {
+        // 设置字体
+        HSSFFont font = workbook.createFont();
+        //设置字体大小
+        //font.setFontHeightInPoints((short)10);
+        //字体加粗
+        //font.setBoldweight(HSSFFont.BOLDWEIGHT_BOLD);
+        //设置字体名字
+        font.setFontName("宋体");
+        //设置样式;
+        HSSFCellStyle style = workbook.createCellStyle();
+        //设置底边框;
+        style.setBorderBottom(HSSFCellStyle.BORDER_THIN);
+        //设置底边框颜色;
+        style.setBottomBorderColor(HSSFColor.BLACK.index);
+        //设置左边框;
+        style.setBorderLeft(HSSFCellStyle.BORDER_THIN);
+        //设置左边框颜色;
+        style.setLeftBorderColor(HSSFColor.BLACK.index);
+        //设置右边框;
+        style.setBorderRight(HSSFCellStyle.BORDER_THIN);
+        //设置右边框颜色;
+        style.setRightBorderColor(HSSFColor.BLACK.index);
+        //设置顶边框;
+        style.setBorderTop(HSSFCellStyle.BORDER_THIN);
+        //设置顶边框颜色;
+        style.setTopBorderColor(HSSFColor.BLACK.index);
+        //在样式用应用设置的字体;
+        style.setFont(font);
+        //设置自动换行;
+        style.setWrapText(false);
+        //设置水平对齐的样式为居中对齐;
+        style.setAlignment(HSSFCellStyle.ALIGN_CENTER);
+        //设置垂直对齐的样式为居中对齐;
+        style.setVerticalAlignment(HSSFCellStyle.VERTICAL_CENTER);
+
+        return style;
+
+    }
+
+}

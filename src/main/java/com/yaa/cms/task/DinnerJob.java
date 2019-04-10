699e06a943c0604e92a1911d0ef22f4f609b9c1e
yangxs <yanghbwork@163.com>
2019-04-10 13:51:59 +0800

    公司晚饭报名任务

--- src/main/java/com/yaa/cms/task/DinnerJob.java
+++ src/main/java/com/yaa/cms/task/DinnerJob.java
@@ -1,6 +1,8 @@
 package com.yaa.cms.task;
 
+import com.alibaba.fastjson.JSONObject;
 import com.yaa.cms.system.model.vo.Response;
+import com.yaa.cms.util.HttpUtils;
 import org.quartz.Job;
 import org.quartz.JobExecutionContext;
 import org.quartz.JobExecutionException;
@@ -9,14 +11,19 @@ import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Component;
 
 @Component
-public class WelcomeJob implements Job {
+public class DinnerJob implements Job {
 
     @Autowired
     SimpMessagingTemplate template;
 
     @Override
     public void execute(JobExecutionContext arg0) throws JobExecutionException {
-        Response response = new Response("这是一个全局任务计划，使用了websocket和quzrtz技术，可以在计划列表中取消!!");
+        String url = "http://58.221.72.190:7089/user/submitEat";
+        String param = "userid=72&currUserid=72&type=2";
+        String result = HttpUtils.sendPost(url,param);
+        JSONObject jsonObject = (JSONObject) JSONObject.parse(result);
+        String msg = jsonObject.getString("msg");
+        Response response = new Response("通知："+msg+"!!");
         template.convertAndSend("/topic/getResponse", response);
     }
 
